336c5c8e49ff0a5a96d064de28db2ab0638db635
Ksr-ccb <harang4282@gmail.com>
2025-05-28 11:51:23 +0900

    chore: initialize project structure

--- src/main/java/com/example/cs25/domain/quiz/exception/QuizException.java
+++ src/main/java/com/example/cs25/domain/quiz/exception/QuizException.java
@@ -1,8 +1,10 @@
 package com.example.cs25.domain.quiz.exception;
 
 import com.example.cs25.global.exception.BaseException;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+@Getter
 public class QuizException extends BaseException {
     private final QuizExceptionCode errorCode;
     private final HttpStatus httpStatus;
