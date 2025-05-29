d2ae5bc809f51bb8459bd62de9377748622e2019
Ksr-ccb <harang4282@gmail.com>
2025-05-29 14:20:46 +0900

    도커에 레디스 설정파일 추가 (#7)
    * chore : initialize project structure (#2)
    
    * chore: initialize project structure
    
    * chore: initialize project structure
    
    * chore: initialize project structure
    
    * fix: 엔티티 오류수정 및 설정파일 오류 수정
    
    * 📝 Add docstrings to `dev` (#3)
    
    Docstrings generation was requested by @Ksr-ccb.
    
    * https://github.com/NBC-finalProject/CS25/pull/2#issuecomment-2914743195
    
    The following files were modified:
    
    * `src/main/java/com/example/cs25/domain/ai/exception/AiException.java`
    * `src/main/java/com/example/cs25/domain/mail/entity/MailLog.java`
    * `src/main/java/com/example/cs25/domain/mail/exception/MailException.java`
    * `src/main/java/com/example/cs25/domain/oauth/exception/OauthException.java`
    * `src/main/java/com/example/cs25/domain/quiz/exception/QuizException.java`
    * `src/main/java/com/example/cs25/domain/userQuizAnswer/entity/UserQuizAnswer.java`
    * `src/main/java/com/example/cs25/domain/userQuizAnswer/exception/UserQuizAnswerException.java`
    * `src/main/java/com/example/cs25/domain/users/entity/User.java`
    * `src/main/java/com/example/cs25/domain/users/exception/UserException.java`
    * `src/main/java/com/example/cs25/domain/users/vo/Subscription.java`
    * `src/main/java/com/example/cs25/global/exception/BaseException.java`
    * `src/main/java/com/example/cs25/global/exception/GlobalExceptionHandler.java`
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * Create run-test.yaml
    
    * Create PULL_REQUEST_TEMPLATE.md
    
    * refactor: ERD 수정으로 인한 Entity 수정 (#4)
    
    * refactor: ERD 수정으로 인한 Entity 수정.
    
    * refactor: ERD 수정으로 인한 Entity 수정2.
    
    * chore: 도커 설정파일에 레디스 추가
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    Co-authored-by: Baek jonghyun <69610809+jong-0126@users.noreply.github.com>
    Co-authored-by: crocusia <132359536+crocusia@users.noreply.github.com>
    Co-authored-by: Kimyoonbeom <101162650+Kimyoonbeom@users.noreply.github.com>

---
+++ src/main/java/com/example/cs25/domain/subscription/entity/Subscription.java
@@ -0,0 +1,38 @@
+package com.example.cs25.domain.subscription.entity;
+
+import com.example.cs25.global.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Subscription extends BaseEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+
+    private boolean isActive = false;
+
+    private int subscriptionType; // "월화수목금토일" => "1111111" , "월수금" => "1010100"
+
+    @Builder
+    public Subscription (String email, LocalDateTime startDate, LocalDateTime endDate, boolean isActive, int subscriptionType){
+        this.email = email;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.isActive = isActive;
+        this.subscriptionType = subscriptionType;
+    }
+}
