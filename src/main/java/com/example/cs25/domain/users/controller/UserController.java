0522d6913909f09cc30c86115e418a8be90f66a5
baegjonghyeon <baek011226@naver.com>
2025-05-30 23:54:02 +0900

    CI/CD AWS 연동

--- src/main/java/com/example/cs25/domain/users/controller/UserController.java
+++ src/main/java/com/example/cs25/domain/users/controller/UserController.java
@@ -1,8 +1,13 @@
 package com.example.cs25.domain.users.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
+    @GetMapping("/users")
+    public String test(){
+        return "test";
+    }
 }
