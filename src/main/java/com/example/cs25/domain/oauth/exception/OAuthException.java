3237a5c83e942e9e7818fecb6ceccc46aa73def5
wannabeing <wannabeing@naver.com>
2025-06-02 15:23:21 +0900

    chore: 원래대로 클래스이름 변경

--- src/main/java/com/example/cs25/domain/oauth/exception/OAuthException.java
+++ src/main/java/com/example/cs25/domain/oauth/exception/OAuthException.java
@@ -2,12 +2,12 @@ package com.example.cs25.domain.oauth.exception;
 
 import org.springframework.http.HttpStatus;
 
-public class OaException {
-    private final OaExceptionCode errorCode;
+public class OAuthException {
+    private final OAuthExceptionCode errorCode;
     private final HttpStatus httpStatus;
     private final String message;
 
-    public OaException(OaExceptionCode errorCode) {
+    public OAuthException(OAuthExceptionCode errorCode) {
         this.errorCode = errorCode;
         this.httpStatus = errorCode.getHttpStatus();
         this.message = errorCode.getMessage();
