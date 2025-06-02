4f87a66b375eaea0123332310ed50aa251d6b069
wannabeing <wannabeing@naver.com>
2025-06-03 02:03:22 +0900

    chore: 구독로그 엔티티에 누락된 컬럼 추가 및 생성자 수정

--- src/main/java/com/example/cs25/domain/subscription/entity/SubscriptionLog.java
+++ src/main/java/com/example/cs25/domain/subscription/entity/SubscriptionLog.java
@@ -1,6 +1,9 @@
 package com.example.cs25.domain.subscription.entity;
 
+import java.time.LocalDate;
+
 import com.example.cs25.domain.quiz.entity.QuizCategory;
+import com.example.cs25.domain.quiz.entity.QuizCategoryType;
 import com.example.cs25.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.Builder;
@@ -16,6 +19,12 @@ public class SubscriptionLog extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(columnDefinition = "DATE")
+    private LocalDate startDate;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate endDate;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id", nullable = false)
     private QuizCategory category;
@@ -27,9 +36,12 @@ public class SubscriptionLog extends BaseEntity {
     private int subscriptionType; // "월화수목금토일" => "1111111" , "월수금" => "1010100"
 
     @Builder
-    public SubscriptionLog(QuizCategory category, Subscription subscription, int subscriptionType){
-        this.category = category;
+    public SubscriptionLog(QuizCategoryType category, Subscription subscription,
+        LocalDate startDate, LocalDate endDate, int subscriptionType){
+        this.category = new QuizCategory(category);
         this.subscription = subscription;
+        this.startDate = startDate;
+        this.endDate = endDate;
         this.subscriptionType = subscriptionType;
     }
 }
