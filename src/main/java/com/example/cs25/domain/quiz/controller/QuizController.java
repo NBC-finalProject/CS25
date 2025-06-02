4f65009217d0a1a033a4c208ac094685e1bb0f2a
crocusia <132359536+crocusia@users.noreply.github.com>
2025-06-02 14:35:58 +0900

    Feat/8 (#19)
    * feat(build.gradle): validation 의존성 추가
    
    * feat : CreateQuizDto 생성
    
    * feat : QuizCategoryRepository 추가
    
    * feat(QuizService) : json 파일 데이터 Quiz 엔티티로 변환 후 저장 기능 추가
    
    * feat : QuizCategory 예외 코드 추가
    
    * feat : uploadQuizJson에 예외 코드 사용' 추가
    
    * feat(QuizController) : quiz 업로드 api 추가
    
    * feat(QuizController) : QuizService의 uploadQuizJson 연동
    
    * Ignore application-local.properties
    
    * feat : 카테고리 타입 생성 api 추가
    
    * refactor(QuizCategoryService) : 메서드 isPresent로 변경
    
    * refactor : 코드래빗 피드백 기반 누락 및 오타 수정
    
    * docker-compose.yml 케시 삭제

--- src/main/java/com/example/cs25/domain/quiz/controller/QuizController.java
+++ src/main/java/com/example/cs25/domain/quiz/controller/QuizController.java
@@ -1,8 +1,40 @@
 package com.example.cs25.domain.quiz.controller;
 
+import com.example.cs25.domain.quiz.entity.QuizCategoryType;
+import com.example.cs25.domain.quiz.entity.QuizFormatType;
+import com.example.cs25.domain.quiz.service.QuizService;
+import com.example.cs25.global.dto.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/quizzes")
 public class QuizController {
 
+    private final QuizService quizService;
+
+    @PostMapping("/upload")
+    public ApiResponse<String> uploadQuizByJsonFile(
+        @RequestParam("file") MultipartFile file,
+        @RequestParam("categoryType") QuizCategoryType categoryType,
+        @RequestParam("formatType") QuizFormatType formatType
+    ) {
+        if (file.isEmpty()) {
+            return new ApiResponse<>(400, "파일이 비어있습니다.");
+        }
+
+        String contentType = file.getContentType();
+        if (contentType == null || !contentType.equals(MediaType.APPLICATION_JSON_VALUE)) {
+            return new ApiResponse<>(400, "JSON 파일만 업로드 가능합니다.");
+        }
+
+        quizService.uploadQuizJson(file, categoryType, formatType);
+        return new ApiResponse<>(200, "문제 등록 성공");
+    }
 }
