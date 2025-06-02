b78a35eb6ef326379216eb07323583f6ed37bffd
wannabeing <wannabeing@naver.com>
2025-06-03 02:02:21 +0900

    feat: 구독/구독로그 예외처리 추가 및 수정

---
+++ src/main/java/com/example/cs25/domain/subscription/exception/SubscriptionLogException.java
@@ -0,0 +1,20 @@
+package com.example.cs25.domain.subscription.exception;
+
+import org.springframework.http.HttpStatus;
+
+import com.example.cs25.global.exception.BaseException;
+
+import lombok.Getter;
+
+@Getter
+public class SubscriptionLogException extends BaseException {
+	private final SubscriptionLogExceptionCode errorCode;
+	private final HttpStatus httpStatus;
+	private final String message;
+
+	public SubscriptionLogException(SubscriptionLogExceptionCode errorCode) {
+		this.errorCode = errorCode;
+		this.httpStatus = errorCode.getHttpStatus();
+		this.message = errorCode.getMessage();
+	}
+}
