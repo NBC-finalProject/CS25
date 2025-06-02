b686fa584dfe78334cdf2f1a48e7c7a26c31e250
wannabeing <wannabeing@naver.com>
2025-06-03 02:01:38 +0900

    chore: 공통응답 DTO 수정
    - `@RequiredArgsConstructor`는 빌더를 사용한다면 추후 삭제해야 함

--- src/main/java/com/example/cs25/global/dto/ApiResponse.java
+++ src/main/java/com/example/cs25/global/dto/ApiResponse.java
@@ -1,16 +1,17 @@
 package com.example.cs25.global.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
 @Builder
+@RequiredArgsConstructor
 public class ApiResponse<T> {
     private final int httpCode;
-    private final T data;
 
-    public ApiResponse(int httpCode, T data) {
-        this.httpCode = httpCode;
-        this.data = data;
-    }
+    @JsonInclude(JsonInclude.Include.NON_NULL) // null 이면 응답 JSON 에서 생략됨
+    private final T data;
 }
