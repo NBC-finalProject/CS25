2391ab9ddc11fb70cee3f90238e9bbed17b4b500
HeeMang-Lee <hemsej018@naver.com>
2025-06-04 10:48:26 +0900

    Fix logging and import issues

--- src/main/java/com/example/cs25/global/dto/AuthUser.java
+++ src/main/java/com/example/cs25/global/dto/AuthUser.java
@@ -1,9 +1,9 @@
 package com.example.cs25.global.dto;
 
-import com.example.cs25.domain.users.entity.Role;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import com.example.cs25.domain.users.entity.Role;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -11,7 +11,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
-import com.example.cs25.domain.users.entity.Role;
 import com.example.cs25.domain.users.entity.User;
 
 @Builder
