d748d4b67d207cd22f5fedde872a07a4a47d3dcf
wannabeing <wannabeing@naver.com>
2025-06-04 12:16:08 +0900

    refactor: 구독테이블에 N+1(QuizCategory) 문제 수정
    문제카테고리(QuizCategory)의 경우, 구독내역이 생성될 때마다 쿼리가 중복되어 발생할 수있다고 판단되어 미리 FetchJoin 설정

--- src/main/java/com/example/cs25/domain/subscription/repository/SubscriptionRepository.java
+++ src/main/java/com/example/cs25/domain/subscription/repository/SubscriptionRepository.java
@@ -1,16 +1,22 @@
 package com.example.cs25.domain.subscription.repository;
 
+import java.util.Optional;
+
 import com.example.cs25.domain.subscription.entity.Subscription;
 import com.example.cs25.domain.subscription.exception.SubscriptionException;
 import com.example.cs25.domain.subscription.exception.SubscriptionExceptionCode;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
 	boolean existsByEmail(String email);
 
+	@Query("SELECT s FROM Subscription s JOIN FETCH s.category WHERE s.id = :id")
+	Optional<Subscription> findByIdWithCategory(Long id);
+
 	default Subscription findByIdOrElseThrow(Long subscriptionId){
-		return findById(subscriptionId)
+		return findByIdWithCategory(subscriptionId)
 			.orElseThrow(() ->
 				new SubscriptionException(SubscriptionExceptionCode.NOT_FOUND_SUBSCRIPTION_ERROR));
 	}
