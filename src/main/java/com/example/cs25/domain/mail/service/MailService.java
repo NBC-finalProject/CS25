62bd0e82edb9b4ecca703c559e3d4bf8890c011f
crocusia <132359536+crocusia@users.noreply.github.com>
2025-06-04 09:32:02 +0900

    feat : MimeMessage 기반 Html형식 메일 전송 메서드 추가

--- src/main/java/com/example/cs25/domain/mail/service/MailService.java
+++ src/main/java/com/example/cs25/domain/mail/service/MailService.java
@@ -12,4 +12,15 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class MailService {
 
+    private final JavaMailSender emailSender; //config 없어도 properties 있으면 자동 생성되므로 autowired 사용도 가능
+
+    public void sendHtmlEmail(String to, String subject, String text) throws MessagingException {
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(text, true); //true 설정 시, HTML 사용 가능
+        emailSender.send(message);
+    }
 }
