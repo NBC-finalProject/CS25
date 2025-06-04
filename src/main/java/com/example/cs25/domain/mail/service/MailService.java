baf20bf5ad999284f132c808217fdc0b21acd209
crocusia <132359536+crocusia@users.noreply.github.com>
2025-06-04 09:25:13 +0900

    feat : 유연성 및 확장성을 위해 MailConfig 추가

---
+++ src/main/java/com/example/cs25/domain/mail/service/MailService.java
@@ -0,0 +1,15 @@
+package com.example.cs25.domain.mail.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MailService {
+
+}
