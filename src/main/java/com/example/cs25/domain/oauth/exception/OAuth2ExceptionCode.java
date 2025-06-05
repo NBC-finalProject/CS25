32d4134e657ceaba2fc2d503a6b0b821dbaa5261
wannabeing <wannabeing@naver.com>
2025-06-06 00:22:52 +0900

    chore: OAuth2 데이터(attributes) 파싱 예외처리 코드 추가

--- src/main/java/com/example/cs25/domain/oauth/exception/OAuth2ExceptionCode.java
+++ src/main/java/com/example/cs25/domain/oauth/exception/OAuth2ExceptionCode.java
@@ -13,7 +13,8 @@ public enum OAuth2ExceptionCode {
     SOCIAL_REQUIRED_FIELDS_MISSING(false, HttpStatus.BAD_REQUEST, "로그인에 필요한 정보가 누락되었습니다."),
     SOCIAL_EMAIL_NOT_FOUND(false, HttpStatus.BAD_REQUEST, "이메일 정보를 가져오지 못하였습니다."),
     SOCIAL_EMAIL_NOT_FOUND_WITH_TOKEN(false, HttpStatus.BAD_REQUEST, "액세스 토큰을 사용했지만 이메일 정보를 찾을 수 없습니다."),
-    SOCIAL_NAME_NOT_FOUND(false, HttpStatus.BAD_REQUEST, "이름(닉네임) 정보를 가져오지 못하였습니다.");
+    SOCIAL_NAME_NOT_FOUND(false, HttpStatus.BAD_REQUEST, "이름(닉네임) 정보를 가져오지 못하였습니다."),
+    SOCIAL_ATTRIBUTES_PARSING_FAILED(false, HttpStatus.BAD_REQUEST, "소셜에서 데이터를 제대로 파싱하지 못하였습니다.");
 
 
     private final boolean isSuccess;
