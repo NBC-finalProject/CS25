c2a33345f01b4685a02c2a347b9aed822059aca6
crocusia <132359536+crocusia@users.noreply.github.com>
2025-06-09 18:56:49 +0900

    feat : MailLogResponseDto 생성

---
+++ src/main/java/com/example/cs25/domain/mail/dto/MailLogResponse.java
@@ -0,0 +1,15 @@
+package com.example.cs25.domain.mail.dto;
+
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class MailLogResponse {
+    private final Long mailLogId;
+    private final Long subscriptionId;
+    private final Long quizId;
+    private final LocalDateTime sendDate;
+    private final String mailStatus;
+}
