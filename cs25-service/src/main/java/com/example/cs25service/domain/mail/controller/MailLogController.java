76aaeac451fcc42deec4ae2375e9071a22779531
crocusia <132359536+crocusia@users.noreply.github.com>
2025-06-17 22:28:06 +0900

    feat : MailLog Controller 추가

--- cs25-service/src/main/java/com/example/cs25service/domain/mail/controller/MailLogController.java
+++ cs25-service/src/main/java/com/example/cs25service/domain/mail/controller/MailLogController.java
@@ -1,5 +1,44 @@
 package com.example.cs25service.domain.mail.controller;
 
-public class MailLogController {
+import com.example.cs25common.global.dto.ApiResponse;
+import com.example.cs25entity.domain.mail.dto.MailLogSearchDto;
+import com.example.cs25service.domain.mail.dto.MailLogResponse;
+import com.example.cs25service.domain.mail.service.MailLogService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/mail-logs")
+@RequiredArgsConstructor
+public class MailLogController {
+    private final MailLogService mailLogService;
+
+    @GetMapping
+    public Page<MailLogResponse> getMailLogs(
+        @RequestBody MailLogSearchDto condition,
+        @PageableDefault(size = 20, sort = "sendDate", direction = Direction.DESC) Pageable pageable
+    ) {
+        return mailLogService.getMailLogs(condition, pageable);
+    }
+
+    @GetMapping("/{id}")
+    public MailLogResponse getMailLog(@PathVariable Long id) {
+        return mailLogService.getMailLog(id);
+    }
+
+    @DeleteMapping
+    public ApiResponse<String> deleteMailLogs(@RequestBody List<Long> ids) {
+        mailLogService.deleteMailLogs(ids);
+        return new ApiResponse<>(200, "MailLog 삭제 완료");
+    }
 }
