12e5f375cd9f777c2d782f172c1cc72e617dd872
crocusia <rainbowsubin0522@gmail.com>
2025-06-18 13:59:10 +0900

    Feat/96 MailLog CRUD 추가 (#102)
    * feat : MailLog 조회용 Dto 생성
    
    * feat : MailLog Repository 추가
    
    * feat : MailLog응답 Dto 추가
    
    * feat : 메일 로그 단일 조회 서비스 추가
    
    * feat : 메일 로그 삭제 기능 추가
    
    * feat : MailLog Controller 추가
    
    * feat : 유효성 검증 및 IllegalArgumentException 예외를 처리하는 글로벌 예외 추가
    
    * feat : 메일 로그 삭제 유효성 검증 추가
    
    * refactor : 일부 코드 수정

--- cs25-entity/src/main/java/com/example/cs25entity/domain/mail/repository/MailLogRepository.java
+++ cs25-entity/src/main/java/com/example/cs25entity/domain/mail/repository/MailLogRepository.java
@@ -1,10 +1,22 @@
 package com.example.cs25entity.domain.mail.repository;
 
 import com.example.cs25entity.domain.mail.entity.MailLog;
+import com.example.cs25entity.domain.mail.exception.CustomMailException;
+import com.example.cs25entity.domain.mail.exception.MailExceptionCode;
+import java.util.Collection;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MailLogRepository extends JpaRepository<MailLog, Long> {
+    Optional<MailLog> findById(Long id);
 
+    default MailLog findByIdOrElseThrow(Long id){
+        return findById(id)
+            .orElseThrow(() ->
+                new CustomMailException(MailExceptionCode.MAIL_LOG_NOT_FOUND_ERROR));
+    }
+
+    void deleteAllByIdIn(Collection<Long> ids);
 }
