02f799ef2a9b0c3528d6bbb829650b8c262f63c7
HeeMang-Lee <hemsej018@naver.com>
2025-06-18 12:04:18 +0900

    fix: 멀티모듈로 변경 후 벡터DB에 문서 적재 안되는 오류 해결 완료 (#101)

---
+++ cs25-entity/src/main/generated/com/example/cs25entity/domain/mail/entity/QMailLog.java
@@ -0,0 +1,58 @@
+package com.example.cs25entity.domain.mail.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QMailLog is a Querydsl query type for MailLog
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QMailLog extends EntityPathBase<MailLog> {
+
+    private static final long serialVersionUID = 1206047030L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QMailLog mailLog = new QMailLog("mailLog");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final com.example.cs25entity.domain.quiz.entity.QQuiz quiz;
+
+    public final DateTimePath<java.time.LocalDateTime> sendDate = createDateTime("sendDate", java.time.LocalDateTime.class);
+
+    public final EnumPath<com.example.cs25entity.domain.mail.enums.MailStatus> status = createEnum("status", com.example.cs25entity.domain.mail.enums.MailStatus.class);
+
+    public final com.example.cs25entity.domain.subscription.entity.QSubscription subscription;
+
+    public QMailLog(String variable) {
+        this(MailLog.class, forVariable(variable), INITS);
+    }
+
+    public QMailLog(Path<? extends MailLog> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QMailLog(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QMailLog(PathMetadata metadata, PathInits inits) {
+        this(MailLog.class, metadata, inits);
+    }
+
+    public QMailLog(Class<? extends MailLog> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.quiz = inits.isInitialized("quiz") ? new com.example.cs25entity.domain.quiz.entity.QQuiz(forProperty("quiz"), inits.get("quiz")) : null;
+        this.subscription = inits.isInitialized("subscription") ? new com.example.cs25entity.domain.subscription.entity.QSubscription(forProperty("subscription"), inits.get("subscription")) : null;
+    }
+
+}
+
---
+++ cs25-entity/src/main/generated/com/example/cs25entity/domain/userQuizAnswer/entity/QUserQuizAnswer.java
@@ -0,0 +1,71 @@
+package com.example.cs25entity.domain.userQuizAnswer.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QUserQuizAnswer is a Querydsl query type for UserQuizAnswer
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QUserQuizAnswer extends EntityPathBase<UserQuizAnswer> {
+
+    private static final long serialVersionUID = -650450628L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QUserQuizAnswer userQuizAnswer = new QUserQuizAnswer("userQuizAnswer");
+
+    public final com.example.cs25common.global.entity.QBaseEntity _super = new com.example.cs25common.global.entity.QBaseEntity(this);
+
+    public final StringPath aiFeedback = createString("aiFeedback");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final BooleanPath isCorrect = createBoolean("isCorrect");
+
+    public final com.example.cs25entity.domain.quiz.entity.QQuiz quiz;
+
+    public final com.example.cs25entity.domain.subscription.entity.QSubscription subscription;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updatedAt = _super.updatedAt;
+
+    public final com.example.cs25entity.domain.user.entity.QUser user;
+
+    public final StringPath userAnswer = createString("userAnswer");
+
+    public QUserQuizAnswer(String variable) {
+        this(UserQuizAnswer.class, forVariable(variable), INITS);
+    }
+
+    public QUserQuizAnswer(Path<? extends UserQuizAnswer> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QUserQuizAnswer(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QUserQuizAnswer(PathMetadata metadata, PathInits inits) {
+        this(UserQuizAnswer.class, metadata, inits);
+    }
+
+    public QUserQuizAnswer(Class<? extends UserQuizAnswer> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.quiz = inits.isInitialized("quiz") ? new com.example.cs25entity.domain.quiz.entity.QQuiz(forProperty("quiz"), inits.get("quiz")) : null;
+        this.subscription = inits.isInitialized("subscription") ? new com.example.cs25entity.domain.subscription.entity.QSubscription(forProperty("subscription"), inits.get("subscription")) : null;
+        this.user = inits.isInitialized("user") ? new com.example.cs25entity.domain.user.entity.QUser(forProperty("user"), inits.get("user")) : null;
+    }
+
+}
+
