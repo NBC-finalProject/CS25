3c02ee21ce035d96ce85ca5e66970318a5fe8769
crocusia <132359536+crocusia@users.noreply.github.com>
2025-06-23 17:47:12 +0900

    chore : 주석 변경

--- cs25-batch/src/main/java/com/example/cs25batch/Cs25BatchApplication.java
+++ cs25-batch/src/main/java/com/example/cs25batch/Cs25BatchApplication.java
@@ -14,9 +14,9 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(
-        scanBasePackages = {
-                "com.example"
-        }
+    scanBasePackages = {
+        "com.example"
+    }
 )
 public class Cs25BatchApplication {
 
@@ -28,43 +28,22 @@ public class Cs25BatchApplication {
     }
 
     @Bean
-    public CommandLineRunner runJob(JobLauncher jobLauncher, 
-                                   ApplicationContext context,
-                                   @Value("${spring.batch.job.name:}") String jobNames) {
-/*        return args -> {
+    public CommandLineRunner runJob(JobLauncher jobLauncher,
+        ApplicationContext context,
+        @Value("${spring.batch.job.name:}") String jobName) {
+        return args -> {
             // 외부에서 Job 이름이 지정된 경우에만 실행
             if (jobName != null && !jobName.isEmpty()) {
                 Job job = context.getBean(jobName, Job.class);
-                
+
                 JobParameters params = new JobParametersBuilder()
                     .addLong("timestamp", System.currentTimeMillis()) // 중복 실행 방지
                     .toJobParameters();
 
                 jobLauncher.run(job, params);
             } else {
-                System.out.println("No job specified. Use --spring.batch.job.name=jobName to run a specific job.");
-            }
-        };*/
-        return args -> {
-            if (jobNames == null || jobNames.isBlank()) {
-                System.out.println("No job specified. Use --spring.batch.job.names=job1,job2 to run jobs.");
-                return;
-            }
-
-            String[] jobNameArray = jobNames.split(",");
-            for (String jobName : jobNameArray) {
-                jobName = jobName.trim();
-                Job job = context.getBean(jobName, Job.class);
-
-                JobParameters params = new JobParametersBuilder()
-                    .addLong("timestamp", System.currentTimeMillis()) // 중복 방지
-                    .toJobParameters();
-
-                System.out.println("Running job: " + jobName);
-                jobLauncher.run(job, params);
-
-                // 시간 차를 두고 실행 (예: 5초)
-                Thread.sleep(10000);
+                System.out.println(
+                    "No job specified. Use --spring.batch.job.name=jobName to run a specific job.");
             }
         };
     }
