68a34d7151ed7bfa19debf37dc5f9f1b6983f6ac
crocusia <132359536+crocusia@users.noreply.github.com>
2025-06-23 18:33:35 +0900

    feat : 메일 발송 실패 원인 글자 수 제한 추가

--- cs25-entity/src/main/java/com/example/cs25entity/domain/mail/entity/MailLog.java
+++ cs25-entity/src/main/java/com/example/cs25entity/domain/mail/entity/MailLog.java
@@ -3,6 +3,7 @@ package com.example.cs25entity.domain.mail.entity;
 import com.example.cs25entity.domain.mail.enums.MailStatus;
 import com.example.cs25entity.domain.quiz.entity.Quiz;
 import com.example.cs25entity.domain.subscription.entity.Subscription;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -41,6 +42,7 @@ public class MailLog {
     @Enumerated(EnumType.STRING)
     private MailStatus status;
 
+    @Column(length = 300)
     private String caused;
     /**
      * Constructs a MailLog entity with the specified id, user, quiz, send date, and mail status.
