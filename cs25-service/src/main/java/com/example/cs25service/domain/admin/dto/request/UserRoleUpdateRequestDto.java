8ecc7665434d053bdc2b3f58745e2ccfbff853fc
Ksr-ccb <harang4282@gmail.com>
2025-06-23 15:44:06 +0900

    refactor: 권한예외 스프링 시큐리티로 변경

--- cs25-service/src/main/java/com/example/cs25service/domain/admin/dto/request/UserRoleUpdateRequestDto.java
+++ cs25-service/src/main/java/com/example/cs25service/domain/admin/dto/request/UserRoleUpdateRequestDto.java
@@ -2,8 +2,10 @@ package com.example.cs25service.domain.admin.dto.request;
 
 import com.example.cs25entity.domain.user.entity.Role;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class UserRoleUpdateRequestDto {
 
     private Role role;
