80f0f4775408be2d901b3fe7b0ba3d7dd52fce36
crocusia <132359536+crocusia@users.noreply.github.com>
2025-06-24 21:31:05 +0900

    feat : QuizResponseDto에 Id 추가

--- cs25-service/src/main/java/com/example/cs25service/domain/quiz/controller/QuizController.java
+++ cs25-service/src/main/java/com/example/cs25service/domain/quiz/controller/QuizController.java
@@ -2,14 +2,11 @@ package com.example.cs25service.domain.quiz.controller;
 
 import com.example.cs25common.global.dto.ApiResponse;
 import com.example.cs25entity.domain.quiz.enums.QuizFormatType;
-import com.example.cs25service.domain.quiz.dto.QuizResponseDto;
 import com.example.cs25service.domain.quiz.service.QuizService;
 import com.example.cs25service.domain.security.dto.AuthUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -42,4 +39,13 @@ public class QuizController {
         quizService.uploadQuizJson(authUser, file, categoryType, formatType);
         return new ApiResponse<>(200, "문제 등록 성공");
     }
+
+    //퀴즈 목록 조회
+
+    //퀴즈 단일 조회
+
+    //퀴즈 내용 수정
+
+    //퀴즈 삭제
+
 }
--- cs25-service/src/main/java/com/example/cs25service/domain/quiz/dto/QuizResponseDto.java
+++ cs25-service/src/main/java/com/example/cs25service/domain/quiz/dto/QuizResponseDto.java
@@ -1,15 +1,19 @@
 package com.example.cs25service.domain.quiz.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class QuizResponseDto {
 
+    private final Long id;
     private final String question;
     private final String answer;
     private final String commentary;
 
-    public QuizResponseDto(String question, String answer, String commentary) {
+    @Builder
+    public QuizResponseDto(Long id, String question, String answer, String commentary) {
+        this.id = id;
         this.question = question;
         this.answer = answer;
         this.commentary = commentary;
