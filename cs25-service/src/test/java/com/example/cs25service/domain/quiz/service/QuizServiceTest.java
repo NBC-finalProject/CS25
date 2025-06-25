7d31766d5ba9adc9af69fa9e46b7f6fe4e23ffb9
crocusia <132359536+crocusia@users.noreply.github.com>
2025-06-25 11:29:46 +0900

    feat : QuizServiceTest 초기 설정

--- cs25-service/src/test/java/com/example/cs25service/domain/quiz/service/QuizServiceTest.java
+++ cs25-service/src/test/java/com/example/cs25service/domain/quiz/service/QuizServiceTest.java
@@ -2,6 +2,29 @@ package com.example.cs25service.domain.quiz.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.example.cs25entity.domain.quiz.repository.QuizCategoryRepository;
+import com.example.cs25entity.domain.quiz.repository.QuizRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import javax.xml.validation.Validator;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 class QuizServiceTest {
+
+    @InjectMocks
+    private QuizService quizService;
+
+    @Mock
+    private ObjectMapper objectMapper;
+    @Mock
+    private Validator validator;
+    @Mock
+    private QuizRepository quizRepository;
+    @Mock
+    private QuizCategoryRepository quizCategoryRepository;
+
 
 }
