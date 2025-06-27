26fffd4ef336232a5cb60682c5f66dd6a1663134
Ksr-ccb <harang4282@gmail.com>
2025-06-27 17:54:16 +0900

    test: getUserQuizAnswerCorrectRate 테스트코드 (#188)

--- cs25-service/src/test/java/com/example/cs25service/domain/profile/service/ProfileServiceTest.java
+++ cs25-service/src/test/java/com/example/cs25service/domain/profile/service/ProfileServiceTest.java
@@ -1,5 +1,8 @@
 package com.example.cs25service.domain.profile.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
 import com.example.cs25entity.domain.quiz.entity.Quiz;
 import com.example.cs25entity.domain.quiz.entity.QuizCategory;
 import com.example.cs25entity.domain.quiz.enums.QuizFormatType;
@@ -17,13 +20,19 @@ import com.example.cs25entity.domain.userQuizAnswer.repository.UserQuizAnswerRep
 import com.example.cs25service.domain.profile.dto.ProfileResponseDto;
 import com.example.cs25service.domain.profile.dto.ProfileWrongQuizResponseDto;
 import com.example.cs25service.domain.profile.dto.UserSubscriptionResponseDto;
-import com.example.cs25service.domain.profile.dto.WrongQuizDto;
 import com.example.cs25service.domain.security.dto.AuthUser;
-import com.example.cs25service.domain.subscription.dto.SubscriptionHistoryDto;
 import com.example.cs25service.domain.subscription.dto.SubscriptionInfoDto;
 import com.example.cs25service.domain.subscription.service.SubscriptionService;
+import com.example.cs25service.domain.userQuizAnswer.dto.CategoryUserAnswerRateResponse;
 import com.example.cs25service.domain.userQuizAnswer.dto.UserQuizAnswerRequestDto;
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -33,13 +42,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.util.ReflectionTestUtils;
-
-import java.time.LocalDate;
-import java.util.*;
-
-import static org.assertj.core.api.Assertions.as;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ProfileServiceTest {
@@ -77,68 +79,68 @@ class ProfileServiceTest {
     @BeforeEach
     void setUp() {
         QuizCategory category = QuizCategory.builder()
-                .categoryType("BACKEND")
-                .build();
+            .categoryType("BACKEND")
+            .build();
 
         subscription = Subscription.builder()
-                .category(category)
-                .email("test@naver.com")
-                .startDate(LocalDate.now())
-                .endDate(LocalDate.now().plusMonths(1))
-                .subscriptionType(EnumSet.of(DayOfWeek.MONDAY, DayOfWeek.TUESDAY))
-                .build();
+            .category(category)
+            .email("test@naver.com")
+            .startDate(LocalDate.now())
+            .endDate(LocalDate.now().plusMonths(1))
+            .subscriptionType(EnumSet.of(DayOfWeek.MONDAY, DayOfWeek.TUESDAY))
+            .build();
         ReflectionTestUtils.setField(subscription, "id", 1L);
 
         subscription1 = Subscription.builder()
-                .category(category)
-                .email("test@naver.com")
-                .startDate(LocalDate.now())
-                .endDate(LocalDate.now().plusMonths(1))
-                .subscriptionType(EnumSet.of(DayOfWeek.SUNDAY, DayOfWeek.MONDAY))
-                .build();
+            .category(category)
+            .email("test@naver.com")
+            .startDate(LocalDate.now())
+            .endDate(LocalDate.now().plusMonths(1))
+            .subscriptionType(EnumSet.of(DayOfWeek.SUNDAY, DayOfWeek.MONDAY))
+            .build();
         ReflectionTestUtils.setField(subscription1, "id", 2L);
         ReflectionTestUtils.setField(subscription, "serialId", "sub-uuid-1");
 
-
         quiz = Quiz.builder()
-                .type(QuizFormatType.MULTIPLE_CHOICE)
-                .question("Java is?")
-                .answer("1. Programming Language")
-                .commentary("Java is a language.")
-                .choice("1. Programming // 2. Coffee")
-                .category(category)
-                .level(QuizLevel.EASY)
-                .build();
+            .type(QuizFormatType.MULTIPLE_CHOICE)
+            .question("Java is?")
+            .answer("1. Programming Language")
+            .commentary("Java is a language.")
+            .choice("1. Programming // 2. Coffee")
+            .category(category)
+            .level(QuizLevel.EASY)
+            .build();
 
         quiz1 = Quiz.builder()
-                .type(QuizFormatType.MULTIPLE_CHOICE)
-                .question("Java is?")
-                .answer("1. Programming Language")
-                .commentary("Java is a language.")
-                .choice("1. Programming // 2. Coffee")
-                .category(category)
-                .level(QuizLevel.EASY)
-                .build();
+            .type(QuizFormatType.MULTIPLE_CHOICE)
+            .question("Java is?")
+            .answer("1. Programming Language")
+            .commentary("Java is a language.")
+            .choice("1. Programming // 2. Coffee")
+            .category(category)
+            .level(QuizLevel.EASY)
+            .build();
 
         authUser = AuthUser.builder()
-                .email("test@naver.com")
-                .name("test")
-                .role(Role.USER)
-                .serialId(serialId)
-                .build();
+            .email("test@naver.com")
+            .name("test")
+            .role(Role.USER)
+            .serialId(serialId)
+            .build();
 
         user = User.builder()
-                .email(authUser.getEmail())
-                .name(authUser.getName())
-                .role(authUser.getRole())
-                .subscription(subscription)
-                .score(3.0)
-                .build();
+            .email(authUser.getEmail())
+            .name(authUser.getName())
+            .role(authUser.getRole())
+            .subscription(subscription)
+            .score(3.0)
+            .build();
         ReflectionTestUtils.setField(user, "id", 1L);
+        ReflectionTestUtils.setField(user, "serialId", "user-uuid-1");
 
         subLogs = List.of(
-                SubscriptionHistory.builder().category(category).subscription(subscription).build(),
-                SubscriptionHistory.builder().category(category).subscription(subscription1).build()
+            SubscriptionHistory.builder().category(category).subscription(subscription).build(),
+            SubscriptionHistory.builder().category(category).subscription(subscription1).build()
         );
     }
 
@@ -148,15 +150,17 @@ class ProfileServiceTest {
         when(userRepository.findBySerialId(authUser.getSerialId())).thenReturn(Optional.of(user));
 
         SubscriptionInfoDto subscriptionInfoDto = SubscriptionInfoDto.builder()
-                .category(subscription.getCategory().getCategoryType())
-                .email(subscription.getEmail())
-                .active(true)
-                .startDate(subscription.getStartDate())
-                .endDate(subscription.getEndDate())
-                .build();
+            .category(subscription.getCategory().getCategoryType())
+            .email(subscription.getEmail())
+            .active(true)
+            .startDate(subscription.getStartDate())
+            .endDate(subscription.getEndDate())
+            .build();
 
-        when(subscriptionService.getSubscription(user.getSubscription().getSerialId())).thenReturn(subscriptionInfoDto);
-        when(subscriptionHistoryRepository.findAllBySubscriptionId(user.getSubscription().getId())).thenReturn(subLogs);
+        when(subscriptionService.getSubscription(user.getSubscription().getSerialId())).thenReturn(
+            subscriptionInfoDto);
+        when(subscriptionHistoryRepository.findAllBySubscriptionId(
+            user.getSubscription().getId())).thenReturn(subLogs);
 
         //when
         UserSubscriptionResponseDto userSubscription = profileService.getUserSubscription(authUser);
@@ -167,9 +171,9 @@ class ProfileServiceTest {
         assertThat(userSubscription.getName()).isEqualTo(user.getName());
         assertThat(userSubscription.getSubscriptionInfoDto()).isEqualTo(subscriptionInfoDto);
         assertThat(userSubscription.getSubscriptionLogPage())
-                .hasSize(2)
-                .extracting("subscriptionId")
-                .containsExactly(subscription.getId(), subscription1.getId());
+            .hasSize(2)
+            .extracting("subscriptionId")
+            .containsExactly(subscription.getId(), subscription1.getId());
     }
 
     @Test
@@ -178,22 +182,25 @@ class ProfileServiceTest {
         when(userRepository.findBySerialId(authUser.getSerialId())).thenReturn(Optional.of(user));
 
         List<UserQuizAnswer> userQuizAnswers = List.of(
-                new UserQuizAnswer("정답1", null, true, user, quiz, subscription),
-                new UserQuizAnswer("정답2", null, false, user, quiz1, subscription)
+            new UserQuizAnswer("정답1", null, true, user, quiz, subscription),
+            new UserQuizAnswer("정답2", null, false, user, quiz1, subscription)
         );
 
-        Page<UserQuizAnswer> page = new PageImpl<>(userQuizAnswers, PageRequest.of(0,10), userQuizAnswers.size());
-        when(userQuizAnswerRepository.findAllByUserId(user.getId(), PageRequest.of(0,10))).thenReturn(page);
+        Page<UserQuizAnswer> page = new PageImpl<>(userQuizAnswers, PageRequest.of(0, 10),
+            userQuizAnswers.size());
+        when(userQuizAnswerRepository.findAllByUserId(user.getId(),
+            PageRequest.of(0, 10))).thenReturn(page);
 
         //when
-        ProfileWrongQuizResponseDto wrongQuiz = profileService.getWrongQuiz(authUser, PageRequest.of(0,10));
+        ProfileWrongQuizResponseDto wrongQuiz = profileService.getWrongQuiz(authUser,
+            PageRequest.of(0, 10));
 
         //then
         assertThat(wrongQuiz.getUserId()).isEqualTo(authUser.getSerialId());
         assertThat(wrongQuiz.getWrongQuizList())
-                .hasSize(1)
-                .extracting("userAnswer")
-                .containsExactly("정답2");
+            .hasSize(1)
+            .extracting("userAnswer")
+            .containsExactly("정답2");
     }
 
     @Test
@@ -209,6 +216,101 @@ class ProfileServiceTest {
         assertThat(profile.getName()).isEqualTo(user.getName());
         assertThat(profile.getRank()).isEqualTo(1);
         assertThat(profile.getScore()).isEqualTo(user.getScore());
+    }
+
+
+    @Nested
+    @DisplayName("getUserQuizAnswerCorrectRate 테스트 ")
+    class getUserQuizAnswerCorrectRateTest {
+
+        private final QuizCategory parentCategory = QuizCategory.builder()
+            .categoryType("BACKEND")
+            .build();
+
+        private User user;
+        private List<QuizCategory> childCategories;
+
+        @BeforeEach
+        void setUp() {
+            user = User.builder()
+                .email(authUser.getEmail())
+                .name(authUser.getName())
+                .role(authUser.getRole())
+                .subscription(subscription)
+                .score(3.0)
+                .build();
+
+            ReflectionTestUtils.setField(user, "id", 1L);
+            ReflectionTestUtils.setField(user, "serialId", "user-uuid-1");
+
+            QuizCategory child1 = QuizCategory.builder().categoryType("SoftwareDesign")
+                .parent(parentCategory).build();
+            QuizCategory child2 = QuizCategory.builder().categoryType("Database")
+                .parent(parentCategory).build();
+            ReflectionTestUtils.setField(child1, "id", 1L);
+            ReflectionTestUtils.setField(child2, "id", 2L);
+            childCategories = List.of(child1, child2);
+
+            ReflectionTestUtils.setField(parentCategory, "children", childCategories);
+        }
+
+        @Test
+        @DisplayName("성공")
+        void getUserQuizAnswerCorrectRate_success() {
+            // given
+            List<UserQuizAnswer> softwareDesignAnswers = List.of(
+                UserQuizAnswer.builder().isCorrect(true).subscription(subscription).quiz(quiz)
+                    .user(user).userAnswer("1").build(),
+                UserQuizAnswer.builder().isCorrect(true).subscription(subscription).quiz(quiz1)
+                    .user(user).userAnswer("2").build()
+            );
+
+            List<UserQuizAnswer> databaseAnswers = List.of(
+                UserQuizAnswer.builder().isCorrect(true).subscription(subscription).quiz(quiz)
+                    .user(user).userAnswer("1").build(),
+                UserQuizAnswer.builder().isCorrect(false).subscription(subscription).quiz(quiz1)
+                    .user(user).userAnswer("2").build()
+            );
+
+            when(userRepository.findBySerialId(authUser.getSerialId())).thenReturn(
+                Optional.of(user));
+            when(quizCategoryRepository.findQuizCategoryByUserId(user.getId())).thenReturn(
+                parentCategory);
+            when(userQuizAnswerRepository.findByUserIdAndQuizCategoryId(user.getId(),
+                1L)).thenReturn(softwareDesignAnswers);
+            when(userQuizAnswerRepository.findByUserIdAndQuizCategoryId(user.getId(),
+                2L)).thenReturn(databaseAnswers);
+
+            // when
+            CategoryUserAnswerRateResponse result = profileService.getUserQuizAnswerCorrectRate(
+                authUser);
+
+            // then
+            assertThat(result.getCorrectRates()).containsEntry("SoftwareDesign", 100.0);
+            assertThat(result.getCorrectRates()).containsEntry("Database", 50.0);
+        }
+
+        @Test
+        @DisplayName("성공 - 퀴즈 로그가 없는 경우 0%로 계산")
+        void getUserQuizAnswerCorrectRate_noAnswers() {
+            // given
+            when(userRepository.findBySerialId(authUser.getSerialId())).thenReturn(
+                Optional.of(user));
+            when(quizCategoryRepository.findQuizCategoryByUserId(user.getId())).thenReturn(
+                parentCategory);
+            when(userQuizAnswerRepository.findByUserIdAndQuizCategoryId(user.getId(),
+                1L)).thenReturn(Collections.emptyList());
+            when(userQuizAnswerRepository.findByUserIdAndQuizCategoryId(user.getId(),
+                2L)).thenReturn(Collections.emptyList());
+
+            // when
+            CategoryUserAnswerRateResponse result = profileService.getUserQuizAnswerCorrectRate(
+                authUser);
+
+            // then
+            assertThat(result.getCorrectRates()).containsEntry("SoftwareDesign", 0.0);
+            assertThat(result.getCorrectRates()).containsEntry("Database", 0.0);
+        }
     }
 
 }
