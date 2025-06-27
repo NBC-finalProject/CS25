f173e58e696f42c0a8133efe82f454759f11c855
hemsej018 <hemsej018@naver.com>
2025-06-27 10:17:48 +0900

    refactor: 잘못된 import문 수정

--- cs25-entity/src/main/java/com/example/cs25entity/domain/userQuizAnswer/repository/UserQuizAnswerRepository.java
+++ cs25-entity/src/main/java/com/example/cs25entity/domain/userQuizAnswer/repository/UserQuizAnswerRepository.java
@@ -1,14 +1,15 @@
 package com.example.cs25entity.domain.userQuizAnswer.repository;
 
 import com.example.cs25entity.domain.userQuizAnswer.entity.UserQuizAnswer;
-import io.lettuce.core.dynamic.annotation.Param;
+
+
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
