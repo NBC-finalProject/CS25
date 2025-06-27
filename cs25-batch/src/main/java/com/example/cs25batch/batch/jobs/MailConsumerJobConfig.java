29ffa748faa53b8465413931b4c892ef68b3fbe4
crocusia <132359536+crocusia@users.noreply.github.com>
2025-06-27 14:53:25 +0900

    refactor : MQ 동기 처리 리팩토링

--- cs25-batch/src/main/java/com/example/cs25batch/batch/jobs/MailConsumerJobConfig.java
+++ cs25-batch/src/main/java/com/example/cs25batch/batch/jobs/MailConsumerJobConfig.java
@@ -1,27 +1,13 @@
 package com.example.cs25batch.batch.jobs;
 
-import com.example.cs25batch.batch.service.BatchProducerService;
-import com.example.cs25batch.batch.service.BatchSubscriptionService;
-import com.example.cs25batch.batch.service.TodayQuizService;
-import com.example.cs25entity.domain.quiz.entity.Quiz;
-import com.example.cs25entity.domain.subscription.dto.SubscriptionMailTargetDto;
-import com.example.cs25entity.domain.subscription.entity.Subscription;
-import com.example.cs25entity.domain.subscription.repository.SubscriptionRepository;
-import java.util.List;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class MailConsumerJobConfig {
--- cs25-batch/src/main/java/com/example/cs25batch/batch/step/MailConsumerStepConfig.java
+++ cs25-batch/src/main/java/com/example/cs25batch/batch/step/MailConsumerStepConfig.java
@@ -1,24 +1,35 @@
 package com.example.cs25batch.batch.step;
 
 import com.example.cs25batch.batch.component.logger.MailStepLogger;
+import com.example.cs25batch.batch.dto.MailDto;
+import com.example.cs25batch.batch.service.TodayQuizService;
+import com.example.cs25batch.context.MailSenderContext;
 import com.example.cs25entity.domain.quiz.entity.Quiz;
-import com.example.cs25entity.domain.subscription.dto.SubscriptionMailTargetDto;
 import com.example.cs25entity.domain.subscription.entity.Subscription;
 import com.example.cs25entity.domain.subscription.repository.SubscriptionRepository;
 import java.util.List;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.stream.MapRecord;
+import org.springframework.data.redis.connection.stream.ReadOffset;
+import org.springframework.data.redis.connection.stream.StreamOffset;
+import org.springframework.data.redis.connection.stream.StreamReadOptions;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class MailConsumerStepConfig {
+
+    @Value("${mail.strategy:sesMailSender}")
+    private String strategyKey;
+
     @Bean
     public Step mailConsumerStep(JobRepository jobRepository,
         @Qualifier("mailConsumerTasklet") Tasklet mailTasklet,
@@ -32,19 +43,32 @@ public class MailConsumerStepConfig {
 
     // TODO: Chunk 방식 고려
     @Bean
-    public Tasklet mailConsumerTasklet(SubscriptionRepository subscriptionRepository) {
+    public Tasklet mailConsumerTasklet(
+        SubscriptionRepository subscriptionRepository,
+        TodayQuizService todayQuizService,
+        MailSenderContext mailSenderContext,
+        StringRedisTemplate redisTemplate
+    ) {
         return (contribution, chunkContext) -> {
+            while (true) {
+                //MQ에서 메시지 꺼내기
+                List<MapRecord<String, Object, Object>> records = redisTemplate.opsForStream().read(
+                    StreamReadOptions.empty().count(1),
+                    StreamOffset.create("quiz-email-stream", ReadOffset.lastConsumed())
+                );
 
-            //long searchStart = System.currentTimeMillis();
-            List<SubscriptionMailTargetDto> subscriptions = subscriptionService.getTodaySubscriptions();
-            //long searchEnd = System.currentTimeMillis();
+                if (records == null || records.isEmpty()) {
+                    break;
+                }
 
-            //log.info("[1. 발송 리스트 조회] {}개, {}ms", subscriptions.size(), searchEnd - searchStart);
-
-            for (SubscriptionMailTargetDto sub : subscriptions) {
-                Long subscriptionId = sub.getSubscriptionId();
-
+                //실제 객체 조회
                 //long getStart = System.currentTimeMillis();
+                MapRecord<String, Object, Object> record = records.get(0);
+
+                String subscriptionIdStr = (String) record.getValue().get("subscriptionId");
+                Long subscriptionId = Long.valueOf(subscriptionIdStr);
+                String recordId = record.getId().getValue();
+
                 Subscription subscription = subscriptionRepository.findByIdOrElseThrow(
                     subscriptionId);
                 //long getEnd = System.currentTimeMillis();
@@ -57,13 +81,20 @@ public class MailConsumerStepConfig {
                     //long quizEnd = System.currentTimeMillis();
                     //log.info("[3. 문제 출제] QuizId : {} {}ms", quiz.getId(), quizEnd - quizStart);
 
+                    MailDto mailDto = MailDto.builder()
+                        .subscription(subscription)
+                        .quiz(quiz)
+                        .build();
+
                     //long mailStart = System.currentTimeMillis();
-                    //mailService.sendQuizEmail(subscription, quiz);
+                    mailSenderContext.send(mailDto, strategyKey);
                     //long mailEnd = System.currentTimeMillis();
                     //log.info("[4. 메일 발송] {}ms", mailEnd - mailStart);
                 }
-            }
 
+                //다 쓴 메시지는 삭제
+                redisTemplate.opsForStream().delete("quiz-email-stream", record.getId());
+            }
             return RepeatStatus.FINISHED;
         };
     }
