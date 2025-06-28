e1839cadc8f0c38b8474b4090f5444363e10d609
ChoiHyuk <wannabeing@naver.com>
2025-06-28 13:52:03 +0900

    Chore: 메일 템플릿을 인라인 스타일과 테이블 기반 레이아웃으로 수정 (#202)
    * chore: 메일 템플릿에 구독설정 링크 추가
    
    * refactor: 메일 템플릿을 인라인 스타일과 테이블 기반 레이아웃으로 수정
    
    * chore: a태그 밑줄 스타일 삭제
    
    * chore: 이메일 제목에 문제 텍스트 넣는걸로 수정
    
    * refactor: 메일템플릿 누락되는 부분 수정
    
    * chore: 메일템플릿에 사용할 로고이미지 추가

--- cs25-batch/src/main/java/com/example/cs25batch/util/MailLinkGenerator.java
+++ cs25-batch/src/main/java/com/example/cs25batch/util/MailLinkGenerator.java
@@ -1,9 +1,13 @@
 package com.example.cs25batch.util;
 
 public class MailLinkGenerator {
-    private static final String DOMAIN = "https://cs25.co.kr/todayQuiz";
+    private static final String DOMAIN = "https://cs25.co.kr";
 
     public static String generateQuizLink(String subscriptionId, String quizId) {
-        return String.format("%s?subscriptionId=%s&quizId=%s", DOMAIN, subscriptionId, quizId);
+        return String.format("%s/todayQuiz?subscriptionId=%s&quizId=%s", DOMAIN, subscriptionId, quizId);
+    }
+
+    public static String generateSubscriptionSettings(String subscriptionId) {
+        return String.format("%s/subscriptions/%s", DOMAIN, subscriptionId);
     }
 }
