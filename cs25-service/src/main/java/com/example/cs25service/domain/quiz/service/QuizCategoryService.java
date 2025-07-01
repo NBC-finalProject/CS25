ef7a1f5a0465f459ab63febd8b6c1b4908c28315
wannabeing <wannabeing@naver.com>
2025-07-02 00:45:03 +0900

    chore: 안쓰는 import 수정

--- cs25-service/src/main/java/com/example/cs25service/domain/quiz/service/QuizCategoryService.java
+++ cs25-service/src/main/java/com/example/cs25service/domain/quiz/service/QuizCategoryService.java
@@ -2,19 +2,12 @@ package com.example.cs25service.domain.quiz.service;
 
 
 import com.example.cs25entity.domain.quiz.entity.QuizCategory;
-import com.example.cs25entity.domain.quiz.exception.QuizException;
-import com.example.cs25entity.domain.quiz.exception.QuizExceptionCode;
 import com.example.cs25entity.domain.quiz.repository.QuizCategoryRepository;
-import com.example.cs25service.domain.quiz.dto.QuizCategoryRequestDto;
-import com.example.cs25service.domain.quiz.dto.QuizCategoryResponseDto;
-import com.example.cs25service.domain.security.dto.AuthUser;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class QuizCategoryService {
@@ -24,8 +17,9 @@ public class QuizCategoryService {
     @Transactional(readOnly = true)
     public List<String> getParentQuizCategoryList() {
         return quizCategoryRepository.findByParentIdIsNull() //대분류만 찾아오도록 변경
-            .stream().map(QuizCategory::getCategoryType
-            ).toList();
+            .stream()
+            .map(QuizCategory::getCategoryType)
+            .toList();
     }
 
 }
