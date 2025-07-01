ab18794497d06417182c43864724ac2e337f709c
Ksr-ccb <harang4282@gmail.com>
2025-07-01 20:21:23 +0900

    fix: getTodayQuizBySubscription 퀴즈뽑기

--- cs25-batch/src/main/java/com/example/cs25batch/batch/controller/QuizTestController.java
+++ cs25-batch/src/main/java/com/example/cs25batch/batch/controller/QuizTestController.java
@@ -1,11 +1,8 @@
 package com.example.cs25batch.batch.controller;
 
-import com.example.cs25batch.batch.dto.QuizDto;
 import com.example.cs25batch.batch.service.TodayQuizService;
 import com.example.cs25common.global.dto.ApiResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +13,12 @@ public class QuizTestController {
 
     private final TodayQuizService accuracyService;
 
-    @GetMapping("/accuracyTest/getTodayQuiz/{subscriptionId}")
-    public ApiResponse<QuizDto> getTodayQuiz(
-        @PathVariable(name = "subscriptionId") Long subscriptionId
-    ) {
-        return new ApiResponse<>(200, accuracyService.getTodayQuiz(subscriptionId));
-    }
+//    @GetMapping("/accuracyTest/getTodayQuiz/{subscriptionId}")
+//    public ApiResponse<QuizDto> getTodayQuiz(
+//        @PathVariable(name = "subscriptionId") Long subscriptionId
+//    ) {
+//        return new ApiResponse<>(200, accuracyService.getTodayQuiz(subscriptionId));
+//    }
 
 //    @GetMapping("/accuracyTest/getTodayQuizNew")
 //    public ApiResponse<QuizDto> getTodayQuizNew() {
