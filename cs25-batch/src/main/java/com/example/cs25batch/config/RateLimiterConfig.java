fd43becdf4c39d3fd6bea6ed8796664eeba8c60c
crocusia <132359536+crocusia@users.noreply.github.com>
2025-07-01 12:15:06 +0900

    feat : Bucket4j RateLimiter Config 추가

---
+++ cs25-batch/src/main/java/com/example/cs25batch/config/RateLimiterConfig.java
@@ -0,0 +1,33 @@
+package com.example.cs25batch.config;
+
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.local.LocalBucket;
+import java.time.Duration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class RateLimiterConfig {
+
+    @Value("${mail.ratelimiter.capacity:14}")
+    private Long capacity;
+
+    @Value("${mail.ratelimiter.millis}")
+    private Long millis;
+
+
+    @Bean(name = "bucketEmail")
+    public Bucket bucketA() {
+        return Bucket.builder()
+            .addLimit(limit ->
+                limit
+                    .capacity(capacity)
+                    .refillIntervally(capacity/2, Duration.ofMillis(500))
+            )
+            .build();
+    }
+}
