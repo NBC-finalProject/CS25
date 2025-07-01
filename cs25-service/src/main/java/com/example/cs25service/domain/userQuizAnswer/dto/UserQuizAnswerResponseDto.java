8efbca59566dc4c17f2293b5f657937c6957f923
wannabeing <wannabeing@naver.com>
2025-07-01 21:37:31 +0900

    refactor: UserQuizAnswerResponseDto 클래스명 변경

---
+++ cs25-service/src/main/java/com/example/cs25service/domain/userQuizAnswer/dto/UserQuizAnswerResponseDto.java
@@ -0,0 +1,20 @@
+package com.example.cs25service.domain.userQuizAnswer.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserQuizAnswerResponseDto {
+    private final Long userQuizAnswerId; // 답변 id
+    private final String question; // 문제
+    private final String answer; // 문제 모범답안
+    private final String commentary; // 문제 해설
+    private boolean isCorrect; // 문제 맞춤 여부
+
+    private final String userAnswer; // 사용자가 답변한 텍스트
+    private final boolean duplicated; // 중복답변 여부
+}
