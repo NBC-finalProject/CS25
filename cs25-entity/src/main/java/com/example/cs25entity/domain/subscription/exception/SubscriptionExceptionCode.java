ea10a740b3165af797526cc49feb2da657009104
Ksr-ccb <harang4282@gmail.com>
2025-07-02 11:53:59 +0900

    fix: 구독 비활성화 후에 문제 못풀게 설정 (#253)

--- cs25-entity/src/main/java/com/example/cs25entity/domain/subscription/exception/SubscriptionExceptionCode.java
+++ cs25-entity/src/main/java/com/example/cs25entity/domain/subscription/exception/SubscriptionExceptionCode.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum SubscriptionExceptionCode {
     ILLEGAL_SUBSCRIPTION_PERIOD_ERROR(false, HttpStatus.BAD_REQUEST, "구독 시작일로부터 1년 이상 구독할 수 없습니다."),
     ILLEGAL_SUBSCRIPTION_TYPE_ERROR(false, HttpStatus.BAD_REQUEST, "요일 값이 비정상적입니다."),
+    DISABLED_SUBSCRIPTION_ERROR(false, HttpStatus.BAD_REQUEST, "비활성화된 구독자 입니다."),
     NOT_FOUND_SUBSCRIPTION_ERROR(false, HttpStatus.NOT_FOUND, "구독 정보를 불러올 수 없습니다."),
     DUPLICATE_SUBSCRIPTION_EMAIL_ERROR(false, HttpStatus.CONFLICT, "이미 구독중인 이메일입니다.");
 
