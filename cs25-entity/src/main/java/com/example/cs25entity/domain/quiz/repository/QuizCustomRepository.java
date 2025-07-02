d5d404e1ee673b8dd81c3102540ea7e2a709a0f7
crocusia <rainbowsubin0522@gmail.com>
2025-07-02 12:23:53 +0900

    Refactor/250 : 프로필 카테고리별 정답률 집계 안되는 문제 해결, Quiz Json 업로드 Dto 수정 (#257)
    * feat :  의존성 추가
    
    * chore : 중복 api 제거로 불필요해진 코드 일부 제거
    
    * chore : 퀴즈 Json 파일 업로드 시, 불필요한 format 제거
    
    * refactor : 정답률 불러오기 해결, 문제 생성 DTO 변결
    
    * chore : 의존성 버전 통일

--- cs25-entity/src/main/java/com/example/cs25entity/domain/quiz/repository/QuizCustomRepository.java
+++ cs25-entity/src/main/java/com/example/cs25entity/domain/quiz/repository/QuizCustomRepository.java
@@ -1,13 +1,10 @@
 package com.example.cs25entity.domain.quiz.repository;
 
-import com.example.cs25entity.domain.quiz.dto.QuizSearchDto;
 import com.example.cs25entity.domain.quiz.entity.Quiz;
 import com.example.cs25entity.domain.quiz.enums.QuizFormatType;
 import com.example.cs25entity.domain.quiz.enums.QuizLevel;
 import java.util.List;
 import java.util.Set;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 public interface QuizCustomRepository {
 
@@ -16,5 +13,4 @@ public interface QuizCustomRepository {
         Set<Long> solvedQuizIds,
         List<QuizFormatType> targetTypes);
 
-    Page<Quiz> searchQuizzes(QuizSearchDto condition, Pageable pageable);
 }
