bde0e0b228aefc8788a399331ede9364a26d1f99
crocusia <132359536+crocusia@users.noreply.github.com>
2025-07-04 21:22:33 +0900

    feat : 재발송을 위한 컨트롤러 추가

--- cs25-service/src/main/java/com/example/cs25service/domain/admin/controller/SendMailController.java
+++ cs25-service/src/main/java/com/example/cs25service/domain/admin/controller/SendMailController.java
@@ -1,20 +1,23 @@
 package com.example.cs25service.domain.admin.controller;
 
 import com.example.cs25common.global.dto.ApiResponse;
-import com.example.cs25entity.domain.quiz.enums.QuizFormatType;
-import java.util.List;
+
+import com.example.cs25service.domain.quiz.dto.QuizCategoryRequestDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/admin")
+@RequiredArgsConstructor
+@RestController
 public class SendMailController {
-
     @PostMapping("/mail-logs/retry")
-    public ApiResponse<String> (
-
+    public ApiResponse<String> createQuizCategory(
+        @Valid @RequestBody QuizCategoryRequestDto request
     ) {
-
+        return new ApiResponse<>(200, "RetryJob Put 성공");
     }
 }
--- cs25-service/src/main/java/com/example/cs25service/domain/admin/dto/request/RetryMailRequestDto.java
+++ cs25-service/src/main/java/com/example/cs25service/domain/admin/dto/request/RetryMailRequestDto.java
@@ -1,5 +1,6 @@
 package com.example.cs25service.domain.admin.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,5 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RetryMailRequestDto {
+
+    @NotEmpty(message = "재발송을 위한 구독 ID 목록은 비어있을 수 없습니다")
     List<Long> subscriptionIds;
 }
