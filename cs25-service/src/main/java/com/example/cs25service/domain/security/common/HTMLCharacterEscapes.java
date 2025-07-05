3895e7c78ec0671e7f170634f3b99187e3281d42
Ksr-ccb <harang4282@gmail.com>
2025-07-05 11:24:13 +0900

    fix: xss 설정

---
+++ cs25-service/src/main/java/com/example/cs25service/domain/security/common/HTMLCharacterEscapes.java
@@ -0,0 +1,37 @@
+package com.example.cs25service.domain.security.common;
+
+import com.fasterxml.jackson.core.SerializableString;
+import com.fasterxml.jackson.core.io.CharacterEscapes;
+import com.fasterxml.jackson.core.io.SerializedString;
+import java.io.Serial;
+import org.apache.commons.lang3.StringEscapeUtils;
+
+public class HTMLCharacterEscapes extends CharacterEscapes {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+    private final int[] asciiEscapes;
+
+    public HTMLCharacterEscapes() {
+        // Define ASCII characters to escape
+        asciiEscapes = CharacterEscapes.standardAsciiEscapesForJSON();
+        asciiEscapes['<'] = CharacterEscapes.ESCAPE_CUSTOM;
+        asciiEscapes['>'] = CharacterEscapes.ESCAPE_CUSTOM;
+        asciiEscapes['&'] = CharacterEscapes.ESCAPE_CUSTOM;
+        asciiEscapes['\"'] = CharacterEscapes.ESCAPE_CUSTOM;
+        asciiEscapes['('] = CharacterEscapes.ESCAPE_CUSTOM;
+        asciiEscapes[')'] = CharacterEscapes.ESCAPE_CUSTOM;
+        asciiEscapes['#'] = CharacterEscapes.ESCAPE_CUSTOM;
+        asciiEscapes['\''] = CharacterEscapes.ESCAPE_CUSTOM;
+    }
+
+    @Override
+    public int[] getEscapeCodesForAscii() {
+        return asciiEscapes;
+    }
+
+    @Override
+    public SerializableString getEscapeSequence(int ch) {
+        return new SerializedString(StringEscapeUtils.escapeHtml4(Character.toString((char) ch)));
+    }
+}
---
+++ cs25-service/src/main/java/com/example/cs25service/domain/security/config/XssConfig.java
@@ -0,0 +1,25 @@
+package com.example.cs25service.domain.security.config;
+
+import com.example.cs25service.domain.security.common.HTMLCharacterEscapes;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+@Configuration
+public class XssConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.getFactory().setCharacterEscapes(new HTMLCharacterEscapes());
+        return objectMapper;
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter jsonEscapeConverter(ObjectMapper objectMapper) {
+        return new MappingJackson2HttpMessageConverter(objectMapper);
+    }
+}
