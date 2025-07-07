80c824e130bcdf03c45be30f7cb0fe473d0c10c9
crocusia <132359536+crocusia@users.noreply.github.com>
2025-07-08 00:51:43 +0900

    chore : 불필요한 import문 제거

--- cs25-service/src/main/java/com/example/cs25service/domain/mailSender/SesMailSenderStrategy.java
+++ cs25-service/src/main/java/com/example/cs25service/domain/mailSender/SesMailSenderStrategy.java
@@ -5,14 +5,12 @@ import com.example.cs25entity.domain.subscription.entity.Subscription;
 import com.example.cs25service.domain.mail.service.SesMailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import software.amazon.awssdk.services.sesv2.SesV2Client;
 
 @RequiredArgsConstructor
 @Component("sesServiceMailSender")
 public class SesMailSenderStrategy implements MailSenderServiceStrategy {
 
     private final SesMailService sesMailService;
-    private final SesV2Client sesV2Client;
 
     @Override
     public void sendVerificationCodeMail(String toEmail, String code) {
