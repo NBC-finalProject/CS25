2d272d673e3c5cfe0e16bdc0fb6cb23eb6095e7a
crocusia <132359536+crocusia@users.noreply.github.com>
2025-07-07 23:56:48 +0900

    feat : service 모듈 ses서비스에 sendquizemail 추가

--- cs25-service/src/main/java/com/example/cs25service/domain/mailSender/SesMailSenderStrategy.java
+++ cs25-service/src/main/java/com/example/cs25service/domain/mailSender/SesMailSenderStrategy.java
@@ -1,14 +1,26 @@
 package com.example.cs25service.domain.mailSender;
 
+import com.example.cs25entity.domain.quiz.entity.Quiz;
+import com.example.cs25entity.domain.subscription.entity.Subscription;
 import com.example.cs25service.domain.mail.service.SesMailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.context.Context;
+import software.amazon.awssdk.services.sesv2.SesV2Client;
+import software.amazon.awssdk.services.sesv2.model.Body;
+import software.amazon.awssdk.services.sesv2.model.Content;
+import software.amazon.awssdk.services.sesv2.model.Destination;
+import software.amazon.awssdk.services.sesv2.model.EmailContent;
+import software.amazon.awssdk.services.sesv2.model.Message;
+import software.amazon.awssdk.services.sesv2.model.SendEmailRequest;
+import software.amazon.awssdk.services.sesv2.model.SesV2Exception;
 
 @RequiredArgsConstructor
 @Component("sesServiceMailSender")
 public class SesMailSenderStrategy implements MailSenderServiceStrategy{
 
     private final SesMailService sesMailService;
+    private final SesV2Client sesV2Client;
 
     @Override
     public void sendVerificationCodeMail(String toEmail, String code) {
