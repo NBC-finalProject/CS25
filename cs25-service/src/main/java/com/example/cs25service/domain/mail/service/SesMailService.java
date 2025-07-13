29639127f97c8d34f71206c05ae43ca64b5e6059
Subin Lee <rainbowsubin0522@gmail.com>
2025-07-13 19:51:34 +0900

    Feat/298 : 첫 구독 시, 바로 문제 풀이 링크 발송 (#299)
    * feat : service 모듈 ses서비스에 sendquizemail 추가
    
    * feat : 전략에 추가
    
    * 메일발송 서비스 추가
    
    * feat : 구독 컨트롤러에 구독 생성 시, 문제 발송 로직 추가
    
    * feat : 메일 템플릿 추가
    
    * chore : 불필요한 import문 제거
    
    * refactor : 퀴즈id 변경

--- cs25-service/src/main/java/com/example/cs25service/domain/mail/service/SesMailService.java
+++ cs25-service/src/main/java/com/example/cs25service/domain/mail/service/SesMailService.java
@@ -2,6 +2,8 @@ package com.example.cs25service.domain.mail.service;
 
 import com.example.cs25entity.domain.mail.exception.CustomMailException;
 import com.example.cs25entity.domain.mail.exception.MailExceptionCode;
+import com.example.cs25entity.domain.quiz.entity.Quiz;
+import com.example.cs25entity.domain.subscription.entity.Subscription;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
@@ -19,8 +21,18 @@ import software.amazon.awssdk.services.sesv2.model.SesV2Exception;
 @RequiredArgsConstructor
 public class SesMailService {
 
+    private static final String DOMAIN = "https://cs25.co.kr";
     private final SpringTemplateEngine templateEngine;
     private final SesV2Client sesV2Client;
+
+    public static String generateQuizLink(String subscriptionId, String quizId) {
+        return String.format("%s/todayQuiz?subscriptionId=%s&quizId=%s", DOMAIN, subscriptionId,
+            quizId);
+    }
+
+    public static String generateSubscriptionSettings(String subscriptionId) {
+        return String.format("%s/subscriptions/%s", DOMAIN, subscriptionId);
+    }
 
     public void sendVerificationCodeEmail(String toEmail, String code) {
         try {
@@ -36,6 +48,59 @@ public class SesMailService {
             //이메일 제목
             Content subject = Content.builder()
                 .data("[CS25] " + "이메일 인증코드")
+                .charset("UTF-8")
+                .build();
+
+            //html 구성
+            Content htmlBody = Content.builder()
+                .data(htmlContent)
+                .charset("UTF-8")
+                .build();
+
+            Body body = Body.builder()
+                .html(htmlBody)
+                .build();
+
+            Message message = Message.builder()
+                .subject(subject)
+                .body(body)
+                .build();
+
+            EmailContent emailContent = EmailContent.builder()
+                .simple(message)
+                .build();
+
+            SendEmailRequest emailRequest = SendEmailRequest.builder()
+                .destination(destination)
+                .content(emailContent)
+                .fromEmailAddress("CS25 <noreply@cs25.co.kr>")
+                .build();
+
+            sesV2Client.sendEmail(emailRequest);
+        } catch (SesV2Exception e) {
+            throw new CustomMailException(MailExceptionCode.EMAIL_SEND_FAILED_ERROR);
+        }
+    }
+
+    public void sendQuizMail(Subscription subscription, Quiz quiz) {
+        try {
+            Context context = new Context();
+            context.setVariable("toEmail", subscription.getEmail());
+            context.setVariable("question", quiz.getQuestion());
+            context.setVariable("quizLink",
+                generateQuizLink(subscription.getSerialId(), quiz.getSerialId()));
+            context.setVariable("subscriptionSettings",
+                generateSubscriptionSettings(subscription.getSerialId()));
+            String htmlContent = templateEngine.process("mail-template", context);
+
+            //수신인
+            Destination destination = Destination.builder()
+                .toAddresses(subscription.getEmail())
+                .build();
+
+            //이메일 제목
+            Content subject = Content.builder()
+                .data("[CS25] " + quiz.getQuestion())
                 .charset("UTF-8")
                 .build();
 
