775e5845de110b9df84947bd25f19daf640ba626
ChoiHyuk <wannabeing@naver.com>
2025-07-24 15:16:19 +0900

    chore: QuizPageService @Transactional readonly 설정하여 조회성능 향상 (#313)

--- cs25-service/src/main/java/com/example/cs25service/domain/quiz/service/QuizPageService.java
+++ cs25-service/src/main/java/com/example/cs25service/domain/quiz/service/QuizPageService.java
@@ -10,9 +10,11 @@ import java.util.Arrays;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class QuizPageService {
 
     private final QuizRepository quizRepository;
