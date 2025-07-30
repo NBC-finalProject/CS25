4c681bcf9e55f9e3bb9e8c29a52dae607a876924
crocusia <132359536+crocusia@users.noreply.github.com>
2025-07-30 17:30:10 +0900

    refactor : 전략키 유효성 검증 로직 별도 메서드로 분리

--- cs25-batch/src/main/java/com/example/cs25batch/sender/context/MailSenderContext.java
+++ cs25-batch/src/main/java/com/example/cs25batch/sender/context/MailSenderContext.java
@@ -3,6 +3,8 @@ package com.example.cs25batch.sender.context;
 import com.example.cs25batch.batch.dto.MailDto;
 import com.example.cs25batch.sender.MailSenderStrategy;
 import java.util.Map;
+
+import io.github.bucket4j.Bucket;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +14,16 @@ public class MailSenderContext {
     private final Map<String, MailSenderStrategy> strategyMap;
 
     public void send(MailDto dto, String strategyKey) {
+        MailSenderStrategy strategy = getValidStrategy(strategyKey);
+        strategy.sendQuizMail(dto);
+    }
+
+    private MailSenderStrategy getValidStrategy(String strategyKey) {
         MailSenderStrategy strategy = strategyMap.get(strategyKey);
         if (strategy == null) {
             throw new IllegalArgumentException("메일 전략이 존재하지 않습니다: " + strategyKey);
         }
-        strategy.sendQuizMail(dto);
+        return strategy;
     }
+
 }
